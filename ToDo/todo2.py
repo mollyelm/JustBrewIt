7b4ab7da732d0583aced95ac4fc2b90ecf8e3fe8
nader <nader.mazen@live.com>
2025-01-02 20:13:36 -0500

    todo2 now can dynamically create potions

--- ToDo/todo2.py
+++ ToDo/todo2.py
@@ -1,15 +1,29 @@
 import sys, random, math
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMainWindow, QVBoxLayout, QColorDialog
+from PyQt6.QtGui import QPixmap, QPainter, QColor, QImage
 
+class TestApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(":]")
+        self.resize(400, 400)
+        self.button = QPushButton("Click me!!!")
+        self.button.clicked.connect(self.onclick)
+        self.setCentralWidget(self.button)
 
-def onclick():
-    arr = ["hello!", "hows it hanging!", "wahoo", "yippee", "i am suffering actively"]  # change this to get different colors!
-    result = get_potion_color(arr)
-    print(result)
-    window.setStyleSheet(f"background-color: #{result}")
+    def onclick(self):
+        arr = ["hello!", "hows it hanging!", "wahoo", "yippee", "i am suffering actively"]  # change this to get different colors!
+        hex = self.get_potion_color(arr)
+        potion = self.recolor_image("potion.png", "#" + hex)
+        pixmap = QPixmap.fromImage(potion)
+        self.label = QLabel()
+        self.label.setPixmap(pixmap)
+        self.label.setWindowTitle("wow a cool looking potion! isnt that neat!")
+        self.label.resize(pixmap.width(), pixmap.height())
+        self.label.show()
 
-def get_potion_color(arr: list[str]):
+    def get_potion_color(self, arr: list[str]):
         for todo in arr:
             vals = [0,0,0,0,0,0]
             for char in todo:
@@ -25,18 +39,27 @@ def get_potion_color(arr: list[str]):
         for val in vals:
             output += hex(val)[2:]
         return output
+    
+    def recolor_image(self, image_path, target_color_hex):
+        image = QImage(image_path)
+        target_color = QColor(target_color_hex)
+        gray_color = QColor(128, 128, 128) # color to be replaced
+        tolerance = 20  # i havent tested it but this should help create a bubbly effect
 
+        for x in range(image.width()):
+            for y in range(image.height()):
+                pixel_color = QColor(image.pixel(x, y))
+                # checks if the pixel matches the gray color within the tolerance
+                if (abs(pixel_color.red() - gray_color.red()) <= tolerance and
+                    abs(pixel_color.green() - gray_color.green()) <= tolerance and
+                    abs(pixel_color.blue() - gray_color.blue()) <= tolerance):
+                    image.setPixelColor(x, y, target_color)
+
+        return image
+
+
+        
 app = QApplication(sys.argv)
-window = QMainWindow()
-
-window.setWindowTitle(":}")
-
-button = QPushButton("Click me!")
-button.clicked.connect(onclick)
-window.setFixedSize(QSize(800,600))
-window.setCentralWidget(button)
-
-
+window = TestApp()
 window.show()
 sys.exit(app.exec())
-
